514c6c145e6e2f2c327fb89cfe780eb196508f79
Eric Carmichael <eric@ckcollab.com>
2015-12-29 19:19:02 -0800

    change absolute site url

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://3-strand-code.github.io/3sc-blog/'
+SITEURL = 'http://blog.3strandcode.com/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
